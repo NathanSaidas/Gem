7c134080d267a0cb0ca6683a0075464ed56cd75b
NathanSaidas <NathanSaidas@users.noreply.github.com>
2014-11-24 21:58:17 -0500

    Changes:
    • Removing Obsolete Project files, Preparing for new Project

--- Project/Common/Core/Entity Component/Engine Components/G_EngineComponents.h
+++
@@ -1,6 +0,0 @@
-#ifndef G_ENGINE_COMPONENTS_H
-#define G_ENGINE_COMPONENTS_H
-
-#include "G_Transform.h"
-
-#endif
--- Project/Common/Core/Utilities/Collections/G_Collections.h
+++
@@ -1,8 +0,0 @@
-#ifndef G_COLLECTIONS_H
-#define G_COLLECTIONS_H
-
-#include "G_ICollection.h"
-#include "G_List.h"
-#include "G_ForwardList.h"
-
-#endif
